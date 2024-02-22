a5a25a8ccfde216d7957003c939af83dfb692283
Anjali Vetcha <160286720+Anjal1i6@users.noreply.github.com>
2024-02-21 18:52:51 -0600

    Add files via upload

---
+++ Anjali Vetcha/script.js
@@ -0,0 +1,5 @@
+document.getElementById('submitButton').addEventListener('click', function() {
+    var name = document.getElementById('nameInput').value;
+    console.log("Welcome " + name);
+  });
+  
