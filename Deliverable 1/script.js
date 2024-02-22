dd02910c3d48641442d082ca1b7a9cb63d7c5916
Anjali Vetcha <160286720+Anjal1i6@users.noreply.github.com>
2024-02-21 18:52:37 -0600

    Delete Deliverable 1 directory

--- Deliverable 1/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('submitButton').addEventListener('click', function() {
-    var name = document.getElementById('nameInput').value;
-    console.log("Welcome " + name);
-  });
-  
